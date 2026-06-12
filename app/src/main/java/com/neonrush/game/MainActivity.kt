3396ffddf10898ad47e66f9569fe7808abcf487b
Jousserwa <elimosjoudin@gmail.com>
2026-06-12 10:15:51 +0300

    Update MainActivity.kt

--- app/src/main/java/com/neonrush/game/MainActivity.kt
+++ app/src/main/java/com/neonrush/game/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import com.neonrush.game.R
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "NeonRushInit"
