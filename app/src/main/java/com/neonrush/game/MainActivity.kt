fe64667ede6a774daa5749462ddd8c144e77636f
Jousserwa <elimosjoudin@gmail.com>
2026-07-03 09:36:53 +0300

    Update MainActivity.kt

--- app/src/main/java/com/neonrush/game/MainActivity.kt
+++ app/src/main/java/com/neonrush/game/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.neonrush.game
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.neonrush.game.db.GameDao
 import com.neonrush.game.ui.NeonRushApp
 import com.neonrush.game.ui.theme.NeonRushTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: NeonRushViewModel by viewModels()
+    private val viewModel: NeonRushViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                val gameDao = GameDao(applicationContext)
+                return NeonRushViewModel(gameDao, applicationContext) as T
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
