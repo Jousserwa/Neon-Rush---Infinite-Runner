725d610497c57eb161e8d84b78fc4bfc014a8df5
Jousserwa <elimosjoudin@gmail.com>
2026-07-03 06:32:32 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("com.revenuecat.purchases:purchases:8.20.0")
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
