c712782cc5b67ff8c10850a28aa3185a1f51c64d
Jousserwa <elimosjoudin@gmail.com>
2026-07-04 07:52:00 +0300

    Update NeonRushApp.kt

--- app/src/main/java/com/neonrush/game/ui/NeonRushApp.kt
+++ app/src/main/java/com/neonrush/game/ui/NeonRushApp.kt
@@ -1229,7 +1229,7 @@ fun RacingSimulatorScreen(simState: SimulationState, viewModel: NeonRushViewMode
     val laserImg = ImageBitmap.imageResource(id = R.drawable.laser1)
     val sawbladeImg = ImageBitmap.imageResource(id = R.drawable.sawblade)
     val droneImg = ImageBitmap.imageResource(id = R.drawable.drone)
-    }
+    
   
 val textMeasurer = rememberTextMeasurer()
     var previousScoreForPopups by remember { mutableStateOf(simState.score) }
