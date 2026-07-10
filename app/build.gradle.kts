02fdfbc3206a08119f1a2f3224f41b42593af520
Jousserwa <elimosjoudin@gmail.com>
2026-07-10 11:25:32 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
