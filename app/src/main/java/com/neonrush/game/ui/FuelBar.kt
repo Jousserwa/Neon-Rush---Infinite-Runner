82f0f9b5a58960a10645bf53d13ba05d191958ea
Jousserwa <elimosjoudin@gmail.com>
2026-07-28 13:21:16 +0300

    Update FuelBar.kt

--- app/src/main/java/com/neonrush/game/ui/FuelBar.kt
+++ app/src/main/java/com/neonrush/game/ui/FuelBar.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
 
 @Composable
 fun FuelBar(
