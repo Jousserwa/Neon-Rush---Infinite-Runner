c7439013b8576ab79854d6d1da3adfcd8854f289
Jousserwa <elimosjoudin@gmail.com>
2026-07-29 10:30:59 +0300

    Update FuelBar.kt

--- app/src/main/java/com/neonrush/game/ui/FuelBar.kt
+++ app/src/main/java/com/neonrush/game/ui/FuelBar.kt
@@ -1,3 +1,4 @@
+
 package com.neonrush.game.ui
 
 import androidx.compose.animation.core.LinearEasing
@@ -16,7 +17,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,7 +31,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
 @Composable
 fun FuelBar(
     fuelPercent: Int,
