4d5b8ccea26798ce1457519466c9a0185b8f2ac4
Jousserwa <elimosjoudin@gmail.com>
2026-08-01 13:49:02 +0300

    Update LocalGameDatabase.kt

--- app/src/main/java/com/neonrush/game/db/LocalGameDatabase.kt
+++ app/src/main/java/com/neonrush/game/db/LocalGameDatabase.kt
@@ -320,5 +320,8 @@ class GameDbHelper(context: Context) : SQLiteOpenHelper(context, "neon_rush_comp
         db.execSQL("ALTER TABLE game_profile ADD COLUMN weeklyRerollCount INTEGER DEFAULT 0")
         db.execSQL("ALTER TABLE game_profile ADD COLUMN monthlyRerollCount INTEGER DEFAULT 0")
     }
+    if (oldVersion < 9) {
+        db.execSQL("ALTER TABLE game_profile ADD COLUMN specialWorldTier INTEGER DEFAULT 0")
+    }
 }
 }
