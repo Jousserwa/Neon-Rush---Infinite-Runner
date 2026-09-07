afbdd734e486cf119aec5ca70990e9e63cf50dc2
Jousserwa <elimosjoudin@gmail.com>
2026-09-07 23:48:41 +0300

    Update NeonRushViewModel.kt

--- app/src/main/java/com/neonrush/game/NeonRushViewModel.kt
+++ app/src/main/java/com/neonrush/game/NeonRushViewModel.kt
@@ -1338,7 +1338,7 @@ fun startRacingSimulation(ghost: GhostChallengeEntity, specialWorldId: Int? = nu
                             when (elem.type) {
                                 "gem" -> {
                                     soundEngine.playGemCollect()
-                                    val multiFactor = if (isMondayGems) 2 else 1
+                                    gemsGathered += 1
                                     gemsGathered += multiFactor
                                     repeat(6) { i ->
                                         activeParticles.add(
