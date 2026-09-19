dc12e4a242b7428a579b5b262384773b5d63c8ad
Jousserwa <elimosjoudin@gmail.com>
2026-09-20 02:14:47 +0300

    Update NeonRushApp.kt

--- app/src/main/java/com/neonrush/game/ui/NeonRushApp.kt
+++ app/src/main/java/com/neonrush/game/ui/NeonRushApp.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.ui.draw.shadow
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.ui.input.pointer.pointerInput
