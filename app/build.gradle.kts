957624a640a19c6af8fc811bb78982498f0526ef
Jousserwa <elimosjoudin@gmail.com>
2026-09-24 10:50:11 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.neonrushinfinite.game"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "1.1.2"
+        versionCode = 23
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
